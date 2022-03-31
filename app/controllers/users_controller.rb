c5e31fee6bf36975687a4b61655b099fd17c7a0e
lp <laxmi.p@simformsolutions.com>
2022-03-31 19:45:11 +0530

    added sidekiq

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,12 +29,15 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to user_path(@user)
-    else
-      flash[:errors]=@user.errors.full_messages
-      redirect_to edit_user_path
-    end
+    @user.update(user_params)
+    TestRunJob.perform_later(@user)
+    redirect_to users_path
+    # if @user.update(user_params)
+    #   redirect_to user_path(@user)
+    # else
+    #   flash[:errors]=@user.errors.full_messages
+    #   redirect_to edit_user_path
+    # end
   end
 
   def destroy
--- app/jobs/test_run_job.rb
+++ app/jobs/test_run_job.rb
@@ -1,7 +1,7 @@
 class TestRunJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user)
+   puts "updated data"
   end
 end
